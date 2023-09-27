f3c39823823235f807c0f65c51e6fd5dbd7d872f
Nitish-Bansal <nitishbansal2000@gmail.com>
2023-09-27 16:53:40 -0500

    clickable

--- src/Counties/CountyCard.tsx
+++ src/Counties/CountyCard.tsx
@@ -1,57 +1,62 @@
-import { Card, CardContent, CardMedia, Typography } from '@mui/material'
-import React from 'react'
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import React from "react";
 import { Link } from "react-router-dom";
 
-const CountyCard = (props:
-  {
-    name: string,
-    imgurl: string,
-    population: string,
-    landarea: string,
-    region: string,
-    precipitation: string,
-  }) => {
+const CountyCard = (props: {
+  name: string;
+  imgurl: string;
+  population: string;
+  landarea: string;
+  region: string;
+  precipitation: string;
+}) => {
   return (
     <Card sx={{ width: 300 }}>
-      <CardContent>
-        <CardMedia
-          component="img"
-          image={props.imgurl}
-          width="10%"
-          height="10%"
-          alt="member image" />
-          
+      <Link
+        to={`CountyInstances/${props.name}`}
+        style={{ textDecoration: "none" }}
+      >
+        <CardContent>
+          <CardMedia
+            component="img"
+            image={props.imgurl}
+            width="10%"
+            height="10%"
+            alt="member image"
+          />
 
-        <Typography variant="h5" component="div" style={{ textAlign: "center" }}>
-          {props.name}
-        </Typography>
+          <Typography
+            variant="h5"
+            component="div"
+            style={{ textAlign: "center" }}
+          >
+            {props.name}
+          </Typography>
 
-        <Typography variant="body1" color="text.secondary">
-          <b>Population: </b> {props.population}
-        </Typography>
+          <Typography variant="body1" color="text.secondary">
+            <b>Population: </b> {props.population}
+          </Typography>
 
-        <Typography variant="body1" color="text.secondary">
-          <b>Land Area: </b> {props.landarea}
-        </Typography>
+          <Typography variant="body1" color="text.secondary">
+            <b>Land Area: </b> {props.landarea}
+          </Typography>
 
-        <Typography variant="body1" color="text.secondary">
-          <b>Region: </b> {props.region}
-        </Typography>
+          <Typography variant="body1" color="text.secondary">
+            <b>Region: </b> {props.region}
+          </Typography>
 
-        <Typography variant="body1" color="text.secondary">
-          <b>Avg Mean Precipitation: </b> {props.precipitation} in.
-        </Typography>
+          <Typography variant="body1" color="text.secondary">
+            <b>Avg Mean Precipitation: </b> {props.precipitation} in.
+          </Typography>
 
-        <li key={props.name}>
-          <Link to = {`CountyInstances/${props.name}`}>{"Learn More"} </Link>
-        </li>
-
-      </CardContent>
+          <li key={props.name}>
+            <Link to={`CountyInstances/${props.name}`}>{"Learn More"} </Link>
+          </li>
+        </CardContent>
+      </Link>
     </Card>
-
-  )
-}
-
+  );
+};
 
 CountyCard.defaultProps = {
   name: "Firstname Lastname",
@@ -59,7 +64,7 @@ CountyCard.defaultProps = {
   population: "0",
   region: "None",
   precipitation: "0",
-  landarea: "0"
-}
+  landarea: "0",
+};
 
-export default CountyCard
+export default CountyCard;
