45166c7b094211e5526a6245c234c09add8a2024
Carolyn Stripling <cjstripling02@gmail.com>
2023-09-27 15:53:19 -0500

    Fixed pipeline failing

--- src/Aid Organizations/AidOrganizationCard.tsx
+++ src/Aid Organizations/AidOrganizationCard.tsx
@@ -1,7 +1,6 @@
 import { Card, CardContent, CardMedia, Typography } from '@mui/material'
 import React from 'react'
 import { Link } from 'react-router-dom'
-import AidOrganizationInstances from '../AidOrganizationInstances'
 
 const AidOrganizationCard = ( props: {
     name: string,
