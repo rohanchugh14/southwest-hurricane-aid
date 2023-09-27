3e4fd1ea5d95041de58555dd2bc9c8aa890fda36
Rohan Chugh <rohanchugh14@gmail.com>
2023-09-27 16:59:57 -0500

    updated instance page for aid organizations

--- src/AidOrganizationInstances/index.tsx
+++ src/AidOrganizationInstances/index.tsx
@@ -1,53 +1,63 @@
-import React from 'react'
-import { useParams } from 'react-router-dom';
-import aidOrganizationData from '../Data/aidorganizations_small.json'
-import { Typography } from '@mui/material';
+import React from "react";
+import { useParams } from "react-router-dom";
+import aidOrganizationData from "../Data/aidorganizations_small.json";
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
-
-
 const AidOrganizationInstances = () => {
-
     const name = useParams().instance;
     const ourOrg = aidOrganizationData.features.find((currObject) => {
         return currObject.attributes.shelter_name === name;
     });
-    const countyLink = ourOrg?.attributes.county_parish.substring(0,1).concat(ourOrg?.attributes.county_parish.toLowerCase().substring(1)) + " County";
+    const countyLink =
+        ourOrg?.attributes.county_parish
+            .substring(0, 1)
+            .concat(
+                ourOrg?.attributes.county_parish.toLowerCase().substring(1)
+            ) + " County";
     return (
         <div>
-            
-            <Typography variant='h2'>{ourOrg?.attributes.shelter_name}</Typography>
-            
-            <img src={"/" + ourOrg?.attributes.imgurl} alt="hurricane" />
-
-            <Typography variant='body1'>
-
-                {ourOrg?.attributes.address_1}, {ourOrg?.attributes.city} TX
-
-                <br/>
-
-                {ourOrg?.attributes.county_parish} COUNTY 
-                <li key={ourOrg?.attributes.county_parish}>
-                    <Link to = {`../../Counties/CountyInstances/${countyLink}`}>{ourOrg?.attributes.county_parish} </Link>
-                </li>
-
-                <br />
-
-                <b>Organization name: </b> {ourOrg?.attributes.org_organization_name}
-
-                <br />
-
-                <b>Phone: </b> {ourOrg?.attributes.org_main_phone !== " " ? ourOrg?.attributes.org_main_phone : "Not listed"}
-
-
-            </Typography>
-
-
-            
-            
-            
+            <Card
+                sx={{
+                    margin: "auto",
+                    width: "50%",
+                    boxShadow:
+                        "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+                    marginTop: "20px",
+                    marginBottom: "20px",
+                }}
+            >
+                <CardMedia
+                    sx={{ height: 300, width: "100%" }}
+                    image={"/" + ourOrg?.attributes.imgurl}
+                    title="organization"
+                />
+                <CardContent>
+                    <Typography variant="h2">
+                        {ourOrg?.attributes.shelter_name}
+                    </Typography>
+                    <Typography variant="body1">
+                        <strong>Address: </strong>
+                        {ourOrg?.attributes.address_1},{" "}
+                        {ourOrg?.attributes.city} TX
+                        <br />
+                        <strong>County: </strong>
+                        <Link to={`/Counties/CountyInstances/${countyLink}`}>
+                            {ourOrg?.attributes.county_parish}{" "}
+                        </Link>
+                        <br />
+                        <b>Organization name: </b>{" "}
+                        {ourOrg?.attributes.org_organization_name}
+                        <br />
+                        <b>Phone: </b>{" "}
+                        {ourOrg?.attributes.org_main_phone !== " "
+                            ? ourOrg?.attributes.org_main_phone
+                            : "Not listed"}
+                    </Typography>
+                </CardContent>
+            </Card>
         </div>
-    )
-}
+    );
+};
 
-export default AidOrganizationInstances
+export default AidOrganizationInstances;
