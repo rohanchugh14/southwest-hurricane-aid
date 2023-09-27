adce4945a2aa7638ace58135efc9a99b95ed34f9
jastew15 <jastew15@gmail.com>
2023-09-27 15:27:00 -0500

    add links

--- src/AidOrganizationInstances/index.tsx
+++ src/AidOrganizationInstances/index.tsx
@@ -2,34 +2,38 @@ import React from 'react'
 import { useParams } from 'react-router-dom';
 import aidOrganizationData from '../Data/aidorganizations_small.json'
 import { Typography } from '@mui/material';
+import { Link } from "react-router-dom";
 
 
 
 const AidOrganizationInstances = () => {
 
     const name = useParams().instance;
-    const ourHurricane = aidOrganizationData.features.find((currObject) => {
+    const ourOrg = aidOrganizationData.features.find((currObject) => {
         return currObject.attributes.shelter_name === name;
     });
-    console.log(ourHurricane)
+    console.log(ourOrg)
     return (
         <div>
             
-            <Typography variant='h2'>{ourHurricane?.attributes.shelter_name}</Typography>
+            <Typography variant='h2'>{ourOrg?.attributes.shelter_name}</Typography>
             
-            <img src={"/" + ourHurricane?.attributes.imgurl} alt="hurricane" />
+            <img src={"/" + ourOrg?.attributes.imgurl} alt="hurricane" />
 
             <Typography variant='body1'>
 
-                {ourHurricane?.attributes.address_1}, {ourHurricane?.attributes.city} TX
+                {ourOrg?.attributes.address_1}, {ourOrg?.attributes.city} TX
 
                 <br/>
 
-                {ourHurricane?.attributes.county_parish} COUNTY 
+                {ourOrg?.attributes.county_parish} COUNTY 
+                <li key={ourOrg?.attributes.county_parish}>
+                    <Link to = {`CountyInstances/${ourOrg?.attributes.county_parish}`}>{ourOrg?.attributes.county_parish} </Link>
+                </li>
 
                 <br />
 
-                <b>Organization name: </b> {ourHurricane?.attributes.org_organization_name}
+                <b>Organization name: </b> {ourOrg?.attributes.org_organization_name}
 
 
             </Typography>
