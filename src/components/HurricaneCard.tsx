44735b0e59ea896aa7dd2b06431f544938dd6f86
Nitish-Bansal <nitishbansal2000@gmail.com>
2023-09-27 15:45:26 -0500

    entire cardaction links to instance

--- src/components/HurricaneCard.tsx
+++ src/components/HurricaneCard.tsx
@@ -1,12 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import Card from "@mui/material/Card";
 import CardActionArea from "@mui/material/CardActionArea";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import Tooltip from "@mui/material/Tooltip";
-import Modal from "@mui/material/Modal";
-import Backdrop from "@mui/material/Backdrop";
-import Fade from "@mui/material/Fade";
 import { Link } from "react-router-dom";
 import CardMedia from "@mui/material/CardMedia";
 
@@ -18,16 +15,6 @@ interface HurricaneDataProps {
 }
 
 export default function HurricaneCard(props: HurricaneDataProps) {
-  const [open, setOpen] = useState(false);
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
   return (
     <div style={{ maxWidth: "100%", height: "100%" }}>
       <Card
@@ -40,56 +27,25 @@ export default function HurricaneCard(props: HurricaneDataProps) {
         }}
       >
         <Tooltip title="Click for More Info">
-          <CardActionArea onClick={handleOpen}>
-            <CardMedia style={{ height: 220 }} image={props.image} />
-            <CardContent>
-              <Typography gutterBottom variant="h5">
-                {props.name}
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                <p>{props.category}</p>
-                <p>Date: {props.date}</p>
-                <li key={props.name}>
-                  <Link to={`HurricaneInstances/${props.name}`}>
-                    {"Learn More"}{" "}
-                  </Link>
-                </li>
-              </Typography>
-            </CardContent>
-          </CardActionArea>
+          <Link
+            to={`HurricaneInstances/${props.name}`}
+            style={{ textDecoration: "none" }}
+          >
+            <CardActionArea>
+              <CardMedia style={{ height: 220 }} image={props.image} />
+              <CardContent>
+                <Typography gutterBottom variant="h5">
+                  {props.name}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  <p>{props.category}</p>
+                  <p>Date: {props.date}</p>
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Link>
         </Tooltip>
       </Card>
-
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        style={{ overflow: "scroll", height: "100%", display: "block" }}
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-      >
-        <Fade in={open}>
-          <div
-            style={{
-              backgroundColor: "#fff",
-              border: "2px solid #000",
-              padding: "16px 32px 24px",
-              width: "50%",
-              marginLeft: "25%",
-              marginRight: "25%",
-            }}
-          >
-            <CardMedia style={{ height: 220 }} image="{props.image}" />
-            <h2 id="transition-modal-title">{props.name}</h2>
-            <p id="transition-modal-description">Category: {props.category}</p>
-            <p id="transition-modal-description">Date: {props.date}</p>
-          </div>
-        </Fade>
-      </Modal>
     </div>
   );
 }
