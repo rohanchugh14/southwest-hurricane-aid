28b7dd1723ab98bc9ea47fed6634d45e33d6eea5
Rohan Chugh <rohanchugh14@gmail.com>
2023-09-26 22:45:55 -0500

    fix unused vars in hurricane instances

--- src/HurricaneInstances/index.tsx
+++ src/HurricaneInstances/index.tsx
@@ -1,21 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import hurricaneData from "../Data/hurricane_data.json";
 import { useParams } from "react-router-dom";
 
 
 
 const HurricaneInstances = () => {
-  const [name, setName] = useState(useParams().instance);
-  console.log(name);
-  // setName(useParams());
+  const name = useParams().instance;
   const ourHurricane = hurricaneData.find((currObject) => {
-    return currObject.Name == name;
+    return currObject.Name === name;
   });
   console.log(ourHurricane)
   return (
     <>
       <h1> {name} </h1>
-      <img src = {ourHurricane?.Image} alt = "no image avaliable" />
+      <img src = {ourHurricane?.Image} alt="hurricane"/>
       <h6> {ourHurricane?.Image} </h6>
       <h1> {ourHurricane?.Category} </h1>
       <h1> {ourHurricane?.Date} </h1>
