f308a9ae67e601c09eece6fcbb264f95b851f388
Eshitha Bangray <eshitha.bangray@gmail.com>
2023-10-10 21:22:10 -0500

    temp home fix

--- frontend/src/Home/index.tsx
+++ frontend/src/Home/index.tsx
@@ -3,14 +3,12 @@ import 'react-slideshow-image/dist/styles.css'
 import { Slide } from 'react-slideshow-image';
 import './Home.css';
 
-import slide1 from './slide1.jpg';
-import slide2 from './slide2.jpg';
-import slide3 from './slide3.jpg';
-import slide4 from './slide4.jpg';
-import slide5 from './slide5.webp';
+import slide1 from './background1.webp';
+import slide2 from './background2.jpg';
+import slide3 from './background3.jpg';
 
 const Home = () => {
-	const images = [slide1, slide2, slide3, slide4, slide5];
+	const images = [slide1, slide2, slide3];
 
 	return (
 		<Slide>
@@ -31,19 +29,6 @@ const Home = () => {
 					<span>Slide 3</span>
 				</div>
 			</div>
-
-			<div className="each-slide-effect">
-				<div style={{ 'backgroundImage': `url(${images[3]})` }}>
-					<span>Slide 4</span>
-				</div>
-			</div>
-
-			<div className="each-slide-effect">
-				<div style={{ 'backgroundImage': `url(${images[4]})` }}>
-					<span>Slide 5</span>
-				</div>
-			</div>
-
 			
 
 		</Slide>
