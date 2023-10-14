2a823b4ef772e49c5d85d2b833998ff09b2fc97f
Carolyn Stripling <cjstripling02@gmail.com>
2023-10-14 16:29:07 -0500

    added a database model for aid organizations

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from flask import Flask, Blueprint, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from models import db
 from models.hurricane import Hurricane
+from models.aidorganization import AidOrganization
 from flask_cors import CORS
 app = Flask(__name__)
 cors = CORS(app)
@@ -57,6 +58,51 @@ def add_hurricane():
     db.session.commit()
 
     return jsonify(new_hurricane.serialize()), 201
+
+
+
+@api_bp.route('/aidorganizations/<int:id>', methods=['GET'])
+def get_organization_by_id(id):
+    org = AidOrganization.query.get(id)
+    if not org:
+        return jsonify({"error": "Hurricane not found"}), 404
+    return jsonify(org.serialize())
+
+
+
+@api_bp.route('/aidorganizations', methods=['POST'])
+def add_organization():
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "No input data provided"}), 400
+    # Validate data here (e.g., check if all required fields are present)
+    print(data)
+    
+    new_org = AidOrganization(
+        shelter_name = data['shelter_name'],
+        address_1 = data['address_1'],
+        city = data['city'],
+        state = data['state'],
+        zipcode = data['zip'],
+        ada_compliant = data['ada_compliant'],
+        wheelchair_accessible = data['wheelchair_accessible'],
+        generator_onsite = data['generator_onsite'],
+        self_sufficient_electricity = data['self_sufficient_electricity'],
+        in_surge_slosh_area = data['in_surge_slosh_area'],
+        org_organization_name = data['org_organization_name'],
+        org_main_phone = data['org_main_phone'],
+        org_email = data['org_email'],
+        score = data['score'],
+        in_100_yr_floodplain = data['in_100_yr_floodplain'],
+        status = data['status'],
+        longitude = data['longitude'],
+        latitude = data['latitude']
+    )
+
+    db.session.add(new_org)
+    db.session.commit()
+
+    return jsonify(new_org.serialize()), 201
 
 app.register_blueprint(api_bp)
 
---
+++ backend/scripts/add_aid_organizations.py
@@ -0,0 +1,31 @@
+import requests
+
+
+def get_orgs():
+    url = "https://gis.fema.gov/arcgis/rest/services/NSS/FEMA_NSS/FeatureServer/5/query?where=state%20%3D%20'TX'&outFields=shelter_name,address_1,city,state,zip,ada_compliant,wheelchair_accessible,generator_onsite,self_sufficient_electricity,in_surge_slosh_area,org_organization_name,org_main_phone,org_email,score,in_100_yr_floodplain,county_parish,status&outSR=4326&f=json"
+    data = requests.get(url).json()
+    
+    features = data["features"]
+    
+    for feature in features:
+        
+        
+        #longitude is x, latitude is y
+        longitude = feature["geometry"]["x"]
+        latitude = feature["geometry"]["y"]
+        
+        if(longitude > -97.86 and longitude < -93.29
+           and latitude > 25.76 and latitude < 30.338):
+        
+            attributes = feature["attributes"]
+            
+            attributes["longitude"] = longitude
+            attributes["latitude"] = latitude
+            
+            print(attributes)
+            response = requests.post("http://localhost:4000/api/aidorganizations", json=attributes)
+
+
+
+if __name__=="__main__": 
+    get_orgs()
