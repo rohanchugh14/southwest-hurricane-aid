ef7c255a154b8fe928bd261336e744c4031172a1
Eshitha Bangray <eshitha.bangray@gmail.com>
2023-10-17 14:12:03 -0500

    Front end unit tests

--- frontend/src/App/App.test.tsx
+++ frontend/src/App/App.test.tsx
@@ -1,9 +1,6 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '.';
 
-test('renders learn react link', () => {
+test('renders the landing page with a navigation bar', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
