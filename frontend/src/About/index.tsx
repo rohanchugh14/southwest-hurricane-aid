589840517f3d81f158e80088aaa476d9b04a129c
Eshitha Bangray <eshitha.bangray@gmail.com>
2023-10-18 20:15:37 -0500

    changed a lot of the about page

--- frontend/src/About/index.tsx
+++ frontend/src/About/index.tsx
@@ -1,4 +1,4 @@
-import { Typography, Grid } from "@mui/material";
+import { Typography, Grid, Button } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import MemberCard from "./MemberCard";
 
@@ -224,6 +224,9 @@ const About = () => {
         </Grid>
       </div>
 
+    {/*------------------------------------------ button changes--------------------------------------------- */ }      
+      <div style={{ margin: "10px" }}>
+      {/* ... (previous code remains unchanged) */}
       <div style={{ paddingLeft: "100px" }}>
         <Typography variant="body1">
           <b>Total number of commits: </b> {commits.length} <br />
@@ -231,44 +234,126 @@ const About = () => {
           <b>Total number of unit tests:</b> 0 <br />
           <br />
         </Typography>
-        <b> Data Sources</b>
-        <ul>
-          <li>
-            <a href="https://www.weather.gov/documentation/services-web-api">
-              https://www.weather.gov/documentation/services-web-api
-            </a>
-          </li>
-          <li>
-            <a href="https://en.wikipedia.org/api/rest_v1/">
-              https://en.wikipedia.org/api/rest_v1/
-            </a>
-          </li>
-          <li>
-            <a href="https://hifld-geoplatform.opendata.arcgis.com/datasets/geoplatform::national-shelter-system-facilities/about">
-              https://hifld-geoplatform.opendata.arcgis.com/datasets/geoplatform::national-shelter-system-facilities/about
-            </a>
-          </li>
-          <li>
-            <a href="https://public.opendatasoft.com/explore/dataset/us-county-boundaries/table/?flg=en-us&disjunctive.statefp&disjunctive.countyfp&disjunctive.name&disjunctive.namelsad&disjunctive.stusab&disjunctive.state_name">
-              https://public.opendatasoft.com/explore/dataset/us-county-boundaries/table/?flg=en-us&disjunctive.statefp&disjunctive.countyfp&disjunctive.name&disjunctive.namelsad&disjunctive.stusab&disjunctive.state_name
-            </a>
-          </li>
-        </ul>
+        
+
+    
+
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
+        <b>Data Sources</b>
+        <br />
+        <Button variant="contained" style={{ backgroundColor: '#B6B7B9', color: '#ffffff', width: '70%', marginBottom: '20px' }} 
+        href="https://www.weather.gov/documentation/services-web-api">
+          Weather API
+        </Button>
+        <Button variant="contained" style={{ backgroundColor: '#B6B7B9', color: '#ffffff', width: '70%', marginBottom: '20px' }} 
+        href="https://en.wikipedia.org/api/rest_v1/">
+          Wikipedia API
+        </Button>
+        
+        <br />
+        <b>Link to GitLab repo </b>
+         <br />
+        <Button variant="contained" style={{ backgroundColor: '#B6B7B9', color: '#ffffff', width: '70%', marginBottom: '20px' }} 
+        href="https://gitlab.com/rohanchugh14/southwest-hurricane-aid">
+          GitLab Repo
+        </Button>
+        
+        <br />
+        <b>Link to Postman API: </b>
+        <br />
+        <Button variant="contained" style={{ backgroundColor: '#B6B7B9', color: '#ffffff', width: '70%', marginBottom: '20px' }} 
+        href="https://documenter.getpostman.com/view/18568319/2s9YJZ3PeF">
+          Postman API
+        </Button>
+      </div>
+
+
+
+
         <b>Tools used:</b>
         <ul>
-          <li>Frontend: React, MaterialUI</li>
+          Frontend
         </ul>
-        <b>Link to GitLab repo: </b>{" "}
-        <a href="https://gitlab.com/rohanchugh14/southwest-hurricane-aid">
-          https://gitlab.com/rohanchugh14/southwest-hurricane-aid
-        </a>
-        <br />
-        <br />
-        <b>Link to Postman API: </b>{" "}
-        <a href="https://documenter.getpostman.com/view/18568319/2s9YJZ3PeF">
-          https://documenter.getpostman.com/view/18568319/2s9YJZ3PeF
-        </a>
+
+        <div style={{ background: '#f0f0f0', padding: '20px', borderRadius: '10px', marginTop: '20px' }}>
+          <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
+            <div style={{ textAlign: 'center' }}>
+              <img src="img/react-logo.png" alt="React Logo" style={{ width: '100px', height: 'auto' }} />
+              <p>React</p>
+            </div>
+            <div style={{ textAlign: 'center' }}>
+              <img src="img/materialUI-logo.png" alt="MaterialUI Logo" style={{ width: '100px', height: 'auto' }} />
+              <p>MaterialUI</p>
+            </div>
+          </div>
+        </div>
+
+        <ul>
+          Backend
+        </ul>
+
+
+        <div style={{ background: '#f0f0f0', padding: '20px', borderRadius: '10px', marginTop: '20px' }}>
+          <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
+            <div style={{ textAlign: 'center' }}>
+              <img src="img/flask-logo.png" alt="Flask Logo" style={{ width: '100px', height: 'auto' }} />
+              <p>Flask</p>
+            </div>
+            <div style={{ textAlign: 'center' }}>
+              <img src="img/sqla-logo.png" alt="SQLAlchemy Logo" style={{ width: '100px', height: 'auto' }} />
+              <p>SQLAlchemy</p>
+            </div>
+            <div style={{ textAlign: 'center' }}>
+              <img src="img/adminer-logo.png" alt="Adminer Logo" style={{ width: '100px', height: 'auto' }} />
+              <p>Adminer</p>
+            </div>
+            <div style={{ textAlign: 'center' }}>
+              <img src="img/postgreSQL-logo.png" alt="PostgreSQL Logo" style={{ width: '100px', height: 'auto' }} />
+              <p>PostgreSQL</p>
+            </div>
+            <div style={{ textAlign: 'center' }}>
+              <img src="img/docker-logo.webp" alt="Docker Logo" style={{ width: '100px', height: 'auto' }} />
+              <p>Docker</p>
+            </div>
+          </div>
+        </div>
+
+
+        <ul>
+          Testing
+        </ul>
+        
+
+        <div style={{ background: '#f0f0f0', padding: '20px', borderRadius: '10px', marginTop: '20px' }}>
+          <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
+            <div style={{ textAlign: 'center' }}>
+              <img src="img/selenium-logo.png" alt="Selenium Logo" style={{ width: '100px', height: 'auto' }} />
+              <p>Selenium</p>
+            </div>
+            <div style={{ textAlign: 'center' }}>
+              <img src="img/jest-logo.avif" alt="Jest Logo" style={{ width: '100px', height: 'auto' }} />
+              <p>Jest</p>
+            </div>
+            <div style={{ textAlign: 'center' }}>
+              <img src="img/unittest-logo.jpg" alt="UnitTest Logo" style={{ width: '100px', height: 'auto' }} />
+              <p>UnitTest</p>
+            </div>
+            <div style={{ textAlign: 'center' }}>
+              <img src="img/postman-logo.png" alt="Postman Logo" style={{ width: '100px', height: 'auto' }} />
+              <p>Postman</p>
+            </div>
+          </div>
+        </div>
+
+        
+        
       </div>
+    </div>
+
+
+
+
+    {/*------------------------------------------ button changes--------------------------------------------- */ }   
     </div>
   );
 };
