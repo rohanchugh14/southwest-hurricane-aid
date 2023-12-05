2385e29ecee3e06362a9359ffbc3f0f929ad6dee
Rohan Chugh <rohanchugh14@gmail.com>
2023-12-04 21:19:06 -0600

    uncomment maps component and update host

--- frontend/src/Aid Organizations/AidOrganizationCard.tsx
+++ frontend/src/Aid Organizations/AidOrganizationCard.tsx
@@ -32,8 +32,7 @@ const AidOrganizationCard = ({ searchTerm, aidOrganization, imgUrl }: Props) => 
                 >
                     <CardActionArea>
                         <CardContent>
-                            {/* <MapsComponent center={{ lat: aidOrganization.latitude, lng: aidOrganization.longitude }} zoom={10} /> */}
-                            {/* TODO: UNCOMMENT ABOVE WHEN TURNING IN */}
+                            <MapsComponent center={{ lat: aidOrganization.latitude, lng: aidOrganization.longitude }} zoom={10} />
                             <Typography gutterBottom variant="h5">
                                 <HighlightedText searchTerm={searchTerm} text={aidOrganization.shelter_name} />
                             </Typography>
